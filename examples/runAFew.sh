7324ea5797c3fef7e32bb9a3166cc0da0c26a046
Steven Bickerton <bick@astro.princeton.edu>
2012-10-01 20:17:42 +0900

    added example sh script to show how to run lsstSim, sdss, and coadd.

---
+++ examples/runAFew.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# lsstSim
+pipeQa.py -e -d -C lsstSim -b ccd -v 890104911 -r 1,1 -c 1,1 krughoff_S12_lsstsim_u_krughoff_2012_0706_183555
+
+# coadd
+pipeQa.py -e -d -z 'all' -T goodSeeing -C coadd -b ccd -k -v '0-250,0-r' daues_S12_sdss_u_s2012prod_sd1000
+
+# sdss
+pipeQa.py -e -F -d -C sdss -b ccd -k -v '1056-200' -c g daues_S12_sdss_u_s2012prod_sd1000 
