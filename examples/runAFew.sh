89c956a25a2d619e59604b98a59206c12cb04fd4
Steven Bickerton <bick@astro.princeton.edu>
2012-10-03 18:05:27 +0900

    bad quotes in runAFew.sh example

--- examples/runAFew.sh
+++ examples/runAFew.sh
@@ -10,10 +10,10 @@ COADD="pipeQa.py -e -d -z 'all' -T goodSeeing -C coadd -b ccd -k -v '0-250,0-r' 
 SDSS="pipeQa.py -e -F -d -C sdss -b ccd -k -v '1056-200' -c g daues_S12_sdss_u_s2012prod_sd1000 "
 
 
-for CMD in "$LSST $COADD $SDSS"
+for CMD in "$LSST" "$COADD" "$SDSS"
 do
     echo '#############################################################################'
     echo $CMD
     echo '#############################################################################'
-    $CMD
+    eval $CMD
 done
